413a0605a2e000347a6aafda802ce97c40dedb36
Alexander Echeverry Torres <alexanderecheverry11@gmail.com>
2020-08-16 15:10:50 -0500

    Estructura basica de la landing y creación del controlador

---
+++ views/landing.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 logo-box">
+            <img class="logo" src="./assets/imagenes/sigma-logo.png" alt="">
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <h1 class="landing-title">
+            Prueba de Desarrollo Sigma
+        </h1>
+        <span class="landing-subtitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta est cupiditate nostrum expedita quam eum non, ab consequuntur eaque velit asperiores iure distinctio cumque esse itaque totam assumenda reiciendis officiis.
+        </span>
+    </div>
+
+
+    <div class="row">
+        <div class="col-md-6">
+            
+        </div>
+        <div class="col-md-6"></div>
+    </div>
+</div>
