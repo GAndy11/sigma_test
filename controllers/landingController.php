9d008e3f031905bd1a88abcc98d267dd1342ba65
Alexander Echeverry Torres <alexanderecheverry11@gmail.com>
2020-08-16 19:23:59 -0500

    Guardar el registro en la BD de SIGMA

--- controllers/landingController.php
+++ controllers/landingController.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once './models/ContactoModel.php';
+
 class landingController
 {
     public function index()
@@ -24,14 +26,35 @@ class landingController
             {
                 $mensaje .= "El campo:  $key no puede quedar vacío <br>";
             }
-
-            echo "<script>$(document).ready(function(){ ToggleMensaje('Mostrar'); });</script>";
-
-            include_once './views/landing.php';
-            exit;
         }
 
+        echo "<script>$(document).ready(function(){ ToggleMensaje('Mostrar'); });</script>";
 
+        if($mensaje != ""){
+            include_once './views/landing.php';
+            echo "<script>$(document).ready(function(){ ToggleMensaje('Mostrar'); });</script>";
+            die();
+        }
+
+        //Crear el contacto
+        $contacto = new contactoModel();
+
+        $contacto->setDepto(trim($_POST['departamento']));
+        $contacto->setCiudad(trim($_POST['ciudad']));
+        $contacto->setNombre(trim($_POST['nombre']));
+        $contacto->setCorreo(trim($_POST['correo']));
         
+        $respuesta = $contacto->GuardarContacto();
+
+        if($respuesta === true){
+            $mensaje = "Registro guardado con éxito";
+        }else{
+            $mensaje = $respuesta;
+        }
+
+        include_once './views/landing.php';
+        echo "<script>$(document).ready(function(){ ToggleMensaje('Mostrar'); });</script>";
+
+
     }
 }
